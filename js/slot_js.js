f4dba859c0943a5ef0e1c308919fb71b5ac119be
jrodr117 <jrodr113@msudenver.edu>
2022-03-09 18:29:45 -0700

    cleaned up code wed 9

--- js/slot_js.js
+++ js/slot_js.js
@@ -1,64 +1,87 @@
+/*----- constants -----*/
 const jackpot = [3,3,3];
+const smallWin = [1,1,1]
+const mediumWin = [2,2,2]
 
-let randomNumsArray = [];
+/*------ state variables ------*/
+let board, money, item1, item2, item3
+
+/*------ cached element references ------*/
+const btnEl = document.querySelector('button');
+const num1El = document.getElementById('num1');
+const num2El = document.getElementById('num2');
+const num3El = document.getElementById('num3');
+const cash = document.getElementById("cash");
+const wmsg = document.getElementById("wmessage");
+const lmsg = document.getElementById("lmessage");
+const jmsg = document.getElementById("jmessage");
+
+/*------ event listener ------*/
+btnEl.addEventListener('click', function(){
+  runNumGenerator();
+});
+
+/*------ functions ------*/
+function init() {
+  board = [null,null,null];
+  // money = 100;
+}
 
 function generateNums() {
     return Math.floor(Math.random() *3 ) + 1;
 }
 
-function subtract() {
-  const cash1 = document.getElementById("cash1");
-  const newCash = parseInt(cash1.value) - 10;
-  cash1.value = newCash;
+function runNumGenerator () {
+  item1 = generateNums();
+  item2 = generateNums();
+  item3 = generateNums();
+  handleNums();
+}
 
-  if (newCash < 10) {
+function handleNums() {
+  num1El.innerHTML = `${item1}`
+  num2El.innerHTML = `${item2}`
+  num3El.innerHTML = `${item3}`
+  render();
+}
+ 
+function render() {
+  board = [item1, item2 ,item3]
+
+  if (board.toString() === jackpot.toString()) {
+    showjMessage();
+  } 
+  else if (item1 === item2 && item1 === item3 && item1 < 3 && board.toString() !== jackpot.toString()) {
+    showwMessage();
+  }
+  else {
+    hideMessage();
+    subtract();
+  }
+}
+
+function subtract() {
+  const newCash = parseInt(cash.value) - 10;
+  cash.value = newCash;
+
+  if (newCash < 5) {
     alert("GAME OVER");
     document.location.reload();
   }
 }
 
-function runNums() {
-    const num1 = document.getElementById('num1');
-    const num2 = document.getElementById('num2');
-    const num3 = document.getElementById('num3');
-    
-    const item1 = generateNums();
-    const item2 = generateNums();
-    const item3 = generateNums();
-
-    num1.innerHTML = `${item1}`
-    num2.innerHTML = `${item2}`
-    num3.innerHTML = `${item3}`
-
-    randomNumsArray = [item1, item2 ,item3]
-    
-    if (randomNumsArray.toString() === jackpot.toString()) {
-      showjMessage();
-    } 
-    else if (item1 === item2 && item1 === item3 && item1 < 3) {
-        showwMessage();
-    }
-    else {
-        hideMessage();
-        subtract();
-    }
-}
-
 function showwMessage() {
-    const wmsg = document.getElementById("wmessage");
-    wmsg.style.display = "block";
+  wmsg.style.display = "block";
 }
 
 function showjMessage() {
-  const jmsg = document.getElementById("jmessage");
   jmsg.style.display = "block";
 }
 
 function hideMessage() {
-    const wmsg = document.getElementById("wmessage");
-    const lmsg = document.getElementById("lmessage");
-    const jmsg = document.getElementById("jmessage");
-    wmsg.style.display = "none"
-    lmsg.style.display = "none"
-    jmsg.style.display = "none"
+  wmsg.style.display = "none"
+  lmsg.style.display = "none"
+  jmsg.style.display = "none"
 }
+
+init()
