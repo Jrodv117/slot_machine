90372d0e3bb158ea91c96ca0341a031adff9bcc9
jrodr117 <jrodr113@msudenver.edu>
2022-03-10 20:33:12 -0700

    updates to style, code, html, readme

---
+++ js/README.md
@@ -0,0 +1,31 @@
+# 1 Royal Loots (A slot machine game by Julian Rodriguez-Vera)
+
+# 1.1 Description:
+
+This slot Machine game gives the user initial credits with the value of 100. The user clicks the "Give me the loot" button and the program returns 3 random numbers. If the numbers dont match 10 credits are subtracted. If the numbers match and arent 333 the user wins 30 credits. If the numbers are 333 the user hits the jackpot and wins the game allowing the user to reset the game. If the user runs out of credits the game is over.
+
+# 1.2 Screenshots:
+
+Clicking [this link](https://i.imgur.com/7lvYeaK.png) will take you to a screenshot of the starting page.
+
+Clicking [this link](https://i.imgur.com/o6yYywQ.png) will take you to a screenshot of spin with matched numbers.
+
+Clicking [this link](https://i.imgur.com/GVnBPIT.png) will take you to a screenshot of a winning jackpot spin.
+
+Clicking [this link](https://i.imgur.com/2mAhgvy.png) will take you to a screenshot of a loss.
+
+# 1.3 Technologies Used:
+
+HTML, CSS, JavaScript.
+
+# 1.4 Getting Started:
+
+Game Link [Royal Loots] https://jrodv117.github.io/slot_machine/
+
+# 1.5 Next Steps:
+
+add a way to allow user to select bet and follow bet
+Add images instead of numbers
+add better styling to enhance user expirience
+add more rows along with more ways to win
+implement a mobile design to support smaller screens
--- js/slot_js.js
+++ js/slot_js.js
@@ -1,87 +1,110 @@
 /*----- constants -----*/
-const jackpot = [3,3,3];
-const smallWin = [1,1,1]
-const mediumWin = [2,2,2]
+const jackpot = [2, 2, 2];
 
 /*------ state variables ------*/
-let board, money, item1, item2, item3
+let board, item1, item2, item3
 
 /*------ cached element references ------*/
-const btnEl = document.querySelector('button');
-const num1El = document.getElementById('num1');
-const num2El = document.getElementById('num2');
-const num3El = document.getElementById('num3');
-const cash = document.getElementById("cash");
-const wmsg = document.getElementById("wmessage");
-const lmsg = document.getElementById("lmessage");
-const jmsg = document.getElementById("jmessage");
+const btnEl = document.getElementById('bttn');
+const refreshBtnEl = document.getElementById('refresh')
+const num1El = document.getElementById('slot1');
+const num2El = document.getElementById('slot2');
+const num3El = document.getElementById('slot3');
+const bank = document.getElementById("bank");
+const winMsg = document.getElementById("winmessage");
+const lossMsg = document.getElementById("lossmessage");
+const jackpotMsg = document.getElementById("jackpotmessage");
 
 /*------ event listener ------*/
-btnEl.addEventListener('click', function(){
-  runNumGenerator();
+btnEl.addEventListener('click', function() {
+    runNumGenerator();
 });
+
+refreshBtnEl.addEventListener('click', function() {
+    document.location.reload();
+    init();
+})
 
 /*------ functions ------*/
 function init() {
-  board = [null,null,null];
-  // money = 100;
+    board = [null, null, null];
+    item1 = 0;
+    item2 = 0;
+    item3 = 0;
+    bank.value =100;
 }
 
 function generateNums() {
-    return Math.floor(Math.random() *3 ) + 1;
+    return Math.floor(Math.random() * 3) + 1;
 }
 
-function runNumGenerator () {
-  item1 = generateNums();
-  item2 = generateNums();
-  item3 = generateNums();
-  handleNums();
+function runNumGenerator() {
+    item1 = generateNums();
+    item2 = generateNums();
+    item3 = generateNums();
+    render();
 }
 
-function handleNums() {
-  num1El.innerHTML = `${item1}`
-  num2El.innerHTML = `${item2}`
-  num3El.innerHTML = `${item3}`
-  render();
-}
- 
 function render() {
-  board = [item1, item2 ,item3]
+    num1El.innerHTML = `${item1}`
+    num2El.innerHTML = `${item2}`
+    num3El.innerHTML = `${item3}`
 
-  if (board.toString() === jackpot.toString()) {
-    showjMessage();
-  } 
-  else if (item1 === item2 && item1 === item3 && item1 < 3 && board.toString() !== jackpot.toString()) {
-    showwMessage();
-  }
-  else {
-    hideMessage();
-    subtract();
-  }
+    board = [item1, item2, item3]
+
+    if (board.toString() === jackpot.toString()) {
+        showJackpotMessage();
+        addJackpot();
+    } else if (item1 === item2 && item1 === item3 && board.toString() !== jackpot.toString()) {
+        showWinMessage();
+        addWin();
+    } else {
+        hideMessage();
+        subtract();
+    }
 }
 
 function subtract() {
-  const newCash = parseInt(cash.value) - 10;
-  cash.value = newCash;
+    const newCash = parseInt(bank.value) - 10;
+    bank.value = newCash;
 
-  if (newCash < 5) {
-    alert("GAME OVER");
-    document.location.reload();
-  }
+    if (newCash < 10) {
+        showLossMessage();
+    }
 }
 
-function showwMessage() {
-  wmsg.style.display = "block";
+function addWin() {
+    const newCash = parseInt(bank.value) + 30;
+    bank.value = newCash;
 }
 
-function showjMessage() {
-  jmsg.style.display = "block";
+function addJackpot() {
+    const newCash = parseInt(bank.value) + 1000;
+    bank.value = newCash;
+    refreshBtnEl.style.display = "block";
+    btnEl.style.display = "none";
+}
+
+function showLossMessage() {
+    lossMsg.style.display = "block";
+    refreshBtnEl.style.display = "block";
+    btnEl.style.display = "none";
+    // document.location.reload();
+}
+
+function showWinMessage() {
+    winMsg.style.display = "block";
+}
+
+function showJackpotMessage() {
+    jackpotMsg.style.display = "block";
 }
 
 function hideMessage() {
-  wmsg.style.display = "none"
-  lmsg.style.display = "none"
-  jmsg.style.display = "none"
+    winMsg.style.display = "none"
+    lossMsg.style.display = "none"
+    jackpotMsg.style.display = "none"
+    refreshBtnEl.style.display = "none"
 }
 
-init()
+init();
